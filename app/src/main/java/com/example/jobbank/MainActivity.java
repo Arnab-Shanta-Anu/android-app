11331c32e59191d811b2f35a2d0595253c91c1d9
arnab <arnabshantaanu@gmail.com>
2019-12-17 21:20:39 +0600

    added validity check for inputs

--- app/src/main/java/com/example/jobbank/MainActivity.java
+++ app/src/main/java/com/example/jobbank/MainActivity.java
@@ -4,35 +4,70 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     Intent intent;
     Button btnLogin;
     TextView tvSignUp;
+    EditText edtEmail,edtPass;
+    private FirebaseAuth mAuth;
+    private String email,pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogin = findViewById(R.id.btnLogIn);
+        mAuth = FirebaseAuth.getInstance();
         intent = new Intent(this, HomeActivity.class);
+
+        btnLogin = findViewById(R.id.btnLogIn);
         tvSignUp = findViewById(R.id.tvSignUp);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPass = findViewById(R.id.edtPass);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(intent);
+                email = edtEmail.getText().toString();
+                pass = edtPass.getText().toString();
+                if(checkValid(email,pass)){
+                    startActivity(intent);
+                }
             }
         });
+
         tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(intent);
             }
         });
+    }
+
+    private boolean checkValid(String email, String password) {
+        if(email.isEmpty()){
+            edtEmail.setError("Enter an email address");
+            edtEmail.requestFocus();
+            return false;
+        }
+        if(password.isEmpty()){
+            edtPass.setError("Enter an password address");
+            edtPass.requestFocus();
+            return false;
+        }
+        if(password.length()<6){
+            edtPass.setError("password needs to be at least 6 characters");
+            edtPass.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 }
