b4b1cad7fb4dc7e8c446593f1cd27a088ce22726
arnab <arnabshantaanu@gmail.com>
2019-12-18 23:23:15 +0600

    nothing

--- app/src/main/java/com/example/jobbank/MainActivity.java
+++ app/src/main/java/com/example/jobbank/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mai);
+        setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
         intent = new Intent(this, HomeActivity.class);
