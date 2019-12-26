64848c90ce4c74857abe780bef200f875ea16c33
arnab <arnabshantaanu@gmail.com>
2019-12-26 15:44:40 +0600

    some tweaks

--- app/src/main/java/com/example/jobbank/MainActivity.java
+++ app/src/main/java/com/example/jobbank/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.jobbank;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,10 +18,10 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    Intent intent;
     EditText edtEmail,edtPass;
     private FirebaseAuth mAuth;
     private String email, password;
+    ProgressDialog progressDialog;
 
     //declare variable
     Button mLoginBtn,mSignUpBtn;
@@ -32,7 +32,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-        intent = new Intent(this, UserProfileActivity.class);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Logging In.....");
 
        //finding btn
         mLoginBtn = findViewById(R.id.loginBtn);
@@ -42,7 +44,16 @@ public class MainActivity extends AppCompatActivity {
         edtEmail = findViewById(R.id.edtEmail);
         edtPass = findViewById(R.id.edtPass);
 
+        //handle login btn click
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = edtEmail.getText().toString();
+                password = edtPass.getText().toString();
 
+                checkValid(email,password);
+            }
+        });
 
         //handle signup btn click
         mSignUpBtn.setOnClickListener(new View.OnClickListener() {
@@ -52,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-/*
+
     private void checkValid(String email, String password) {
         if(email.isEmpty()){
             edtEmail.setError("Enter an email address");
@@ -76,24 +87,23 @@ public class MainActivity extends AppCompatActivity {
             edtEmail.requestFocus();
             return ;
         }
-        mAuth.createUserWithEmailAndPassword(email, password)
+        progressDialog.show();
+        mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
 
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         //Toast.makeText(getApplicationContext(), "in OnComplete",Toast.LENGTH_SHORT).show();
-
+                        progressDialog.dismiss();
                         if (task.isSuccessful()) {
                             edtEmail.setText("success");
                             Toast.makeText(getApplicationContext(), "auth success",Toast.LENGTH_SHORT).show();
-                            startActivity(intent);
+                            startActivity(new Intent(MainActivity.this, UserProfileActivity.class));
                         } else {
                             Toast.makeText(getApplicationContext(), "auth failed",Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
-
-
- */
     }
+}
 
